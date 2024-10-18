44565f443d1845940bad976cb72c2e767e59a135
MassimoCorvo <massimocorvo22@gmail.com>
2024-10-18 17:39:52 +0200

    palindrome

---
+++ palindroma/palindroma.js
@@ -0,0 +1,44 @@
+console.log("collegato");
+
+const word = prompt("Inserire parola");
+
+console.log(word);
+
+const result = isPalindrome(word);
+console.log(result);
+
+
+
+//FUNCTION
+/**
+ * Verifica se la parola è palindroma
+ * @param {string} wordToCheck
+ * @returns {string}
+ */
+
+function isPalindrome(wordToCheck) {
+
+    const wordArray = wordToCheck.split("");
+    console.log(wordArray);
+
+    const wordArrayLength = wordArray.length;
+
+    const wordArrayReverse = wordArray.toReversed();
+    console.log(wordArrayReverse);
+
+    let result = "E' palindroma";
+
+    for(let i = 0; i < wordArrayLength; i++){
+
+        if(wordArray[i] !== wordArrayReverse[i])
+        {   
+            result = "Non è palindroma";
+            return result;
+        }
+
+    }
+    
+     return result;
+
+}
+
