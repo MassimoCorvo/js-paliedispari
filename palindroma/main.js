f4c324a6aa9d1ce79780abeefeae275b018e166c
MassimoCorvo <massimocorvo22@gmail.com>
2024-10-21 14:37:26 +0200

    final version

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,11 +1,13 @@
-console.log("collegato");
-
 const word = prompt("Inserire parola");
 
 console.log(word);
 
 const result = isPalindrome(word);
 console.log(result);
+
+const print = document.getElementById("result");
+print.innerText = result;
+
 
 
 
@@ -18,6 +20,9 @@ console.log(result);
 
 function isPalindrome(wordToCheck) {
 
+    //Soluzione con metodi built-in
+
+    /*
     const wordArray = wordToCheck.split("");
     const wordArrayLength = wordArray.length;
     const wordArrayReverse = wordArray.toReversed();
@@ -34,7 +39,37 @@ function isPalindrome(wordToCheck) {
 
     }
     
-     return result;
+     return result; */
+
+     //Soluzione #2
+
+     let result = "E' palindroma";
+     const wordToCheckLowerCase = wordToCheck.toLowerCase();
+     const wordLength = wordToCheck.length;
+
+     if(wordLength === 0)
+        return "Non è palindroma";
+
+     if(wordLength === 1 && wordToCheck !== " ")
+        return result;
+
+     else if ( wordToCheck === " " || wordToCheck === "" ){
+        return "Non è palindroma";
+     }
+
+     else {
+        let j = wordLength - 1;
+
+        for(let i = 0; i < j; i++){
+            if( wordToCheckLowerCase[i] !== wordToCheckLowerCase[j])
+                {return "Non è palindroma";}
+
+            j--;
+        }
+
+        return result;
+     }
+
 
 }
 
--- paridispari/main.js
+++ paridispari/main.js
@@ -1,33 +1,48 @@
 let choice = prompt("Scegli pari o dispari");
+const print = document.querySelector("body");
 
-while ( choice.toLowerCase() !== "pari"  && choice.toLowerCase() !== "dispari" )
-{
+while (choice.toLowerCase() !== "pari" && choice.toLowerCase() !== "dispari") {
     console.log("Scelta non valida");
     choice = prompt("Scegli pari o dispari");
 }
+
+print.innerHTML += "Scelta: " + choice.toLowerCase() + "<br/>";
 console.log("Scelta: ", choice);
 
 let number = Number(prompt("Scegli un numero da 1 a 5"));
 
-while ( number < 1 || number > 5 )
-    {
-        console.log("Scelta non valida");
-        number = Number(prompt("Scegli un numero da 1 a 5"));
-    }
+while (number < 1 || number > 5 || isNaN(number)) {
+    console.log("Scelta non valida");
+    number = Number(prompt("Scegli un numero da 1 a 5"));
+}
+
+print.innerHTML += "Numero scelto: " + number + "<br/>";
 
 console.log("Numero scelto: ", number);
 
 const randomNumber = getRndInteger(1, 5);
 console.log("Random: ", randomNumber);
 
+print.innerHTML += "Numero random: " + randomNumber + "<br/>";
+
 const finalSum = sum(number, randomNumber);
 console.log("Somma: ", finalSum);
 
-if( evenOdd(finalSum) === choice.toLowerCase() ){
-    console.log("Hai vinto");}
+print.innerHTML += "Somma: " + finalSum + "<br/>";
+
+let result;
+
+if (evenOdd(finalSum) === choice.toLowerCase()) {
+    console.log("Hai vinto");
+    result = "Hai vinto";
+
+}
 else {
     console.log("Hai perso");
+    result = "Hai perso";
 }
+
+printResult(result);
 
 
 //FUNCTIONS
@@ -64,6 +79,29 @@ function getRndInteger(min, max) {
  * @param {number} num2
  * @returns {number}
  */
-function sum(num1, num2){
+function sum(num1, num2) {
     return num1 + num2;
+}
+
+/**
+ * Funzione che crea l'elemento HTML con dentro il risultato
+ * @param {string} result
+ */
+function printResult(result) {
+
+    const print = document.createElement("span");
+
+    if (result == "Hai vinto") {
+        print.append(result);
+        print.classList.add("text-success");
+    }
+
+    else {
+        print.append(result);
+        print.classList.add("text-danger");
+    }
+
+    print.classList.add("fs-1", "fst-italic");
+    document.body.appendChild(print);
+
 }
